da9c44b73c30ecb18e36345c283df08763565677
Yauheni Tsiarokhin <hihihiko00@gmail.com>
2018-08-05 23:38:58 +0300

    start organizing OTP app

---
+++ src/file_util.erl
@@ -0,0 +1,48 @@
+-module(file_util).
+-export([list_dir_recursive/1]).
+
+-include("../include/records.hrl").
+
+list_dir_recursive(DirName) ->
+    DeepFileList = do_list_dir_recursive(DirName),
+    lists:map(fun({ok, FileName}) -> FileName end,
+              lists:flatten(DeepFileList)).
+
+process_file(FileName) ->
+    Size = get_size(FileName),
+    Lines = count_lines(FileName),
+    Type = get_filetype(FileName),
+    #file_info{name=FileName, size=Size, lines=Lines, type=Type}.
+
+count_lines(FileName) ->
+    {ok, Device} = file:open(FileName, [read]),
+    do_count_lines(Device, 0).
+
+get_filetype(FileName) -> 
+    case filename:extension(FileName) of
+        [] -> "no filetype";
+        FileType -> FileType
+    end.
+
+get_size(FileName) -> filelib:file_size(FileName).
+
+do_list_dir_recursive(BaseName) ->
+    {ok, FileList} = file:list_dir(BaseName),
+    ExtendedFileList = lists:map(fun(FileName) ->
+                                     add_basename(BaseName, FileName)
+                                 end, FileList),
+    lists:map(fun (FileName) ->
+                  case filelib:is_dir(FileName) of
+                      true -> do_list_dir_recursive(FileName);
+                      false -> {ok, process_file(FileName)}
+                  end
+              end,
+              ExtendedFileList).
+
+do_count_lines(Device, Result) ->
+    case io:get_line(Device, "") of
+        eof -> file:close(Device), Result;
+        _Line -> do_count_lines(Device, Result + 1)
+    end.
+
+add_basename(BaseName, FileName) -> BaseName ++ "/" ++ FileName.
---
+++ src/line_server.erl
@@ -0,0 +1,49 @@
+-module(line_server).
+-behaviour(gen_server).
+
+%% DEBUG
+-export([state/0]).
+
+-export([start_link/1, start_link/0, stop/0,
+         change_dir/1]).
+
+-export([init/1, handle_call/3, handle_cast/2,
+         handle_info/2, terminate/2, code_change/3]).
+
+-define(SERVER, ?MODULE).
+-define(DEFAULT_DIR, "~/.linecounter").
+
+-record(state, {dir_name}).
+
+start_link(Dir) ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [Dir], []).
+
+start_link() ->
+    start_link(?DEFAULT_DIR).
+
+stop() ->
+    gen_server:cast(?SERVER, stop).
+
+change_dir(NewDir) ->
+    gen_server:cast(?SERVER, {change_dir, NewDir}).
+
+%% DEBUG
+state() ->
+    gen_server:call(?SERVER, state).
+
+init([Dir]) -> {ok, #state{dir_name=Dir}}.
+
+%% DEBUG
+handle_call(state, _From, State) ->
+    {reply, State, State}.
+
+handle_cast({change_dir, NewDir}, State) ->
+    {noreply, State#state{dir_name=NewDir}};
+handle_cast(stop, State) ->
+    {stop, normal, State}.
+
+handle_info(_Msg, State) -> {noreply, State}.
+
+terminate(_Reason, _State) -> ok.
+
+code_change(_OldVsn, State, _Extra) -> {ok, State}.
