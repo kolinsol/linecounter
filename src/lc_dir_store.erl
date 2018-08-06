9c6fad68a8ff0b7a2f623cd40d5864ed6932436b
Yauheni Tsiarokhin <hihihiko00@gmail.com>
2018-08-07 00:05:04 +0300

    refactored store

---
+++ src/lc_dir_store.erl
@@ -0,0 +1,23 @@
+-module(lc_store).
+
+-export([init/0, insert/2,
+         delete/1, lookup/1]).
+
+-define(STORE, "~/.linecounter").
+-define(TABLE_NAME, ?MODULE).
+
+init() ->
+    ets:new(?TABLE_NAME, [public, named_table]),
+    ok.
+
+insert(DirName, Pid) ->
+    ets:insert(?TABLE_NAME, {DirName, Pid}).
+
+lookup(DirName) ->
+    case ets:lookup(?TABLE_NAME, DirName) of
+         [{DirName, Pid}] -> {DirName, Pid};
+         [] -> {error, not_found}
+    end.
+
+delete(Pid) ->
+    ets:match_delete(?TABLE_NAME, {'_', Pid}).
