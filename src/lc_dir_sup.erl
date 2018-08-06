d9d12780c9755da692b6e4c61cb0b40003df943e
Yauheni Tsiarokhin <hihihiko00@gmail.com>
2018-08-06 08:09:47 +0300

    add lover level supervision tree skeleton

---
+++ src/lc_dir_sup.erl
@@ -0,0 +1,23 @@
+-module(lc_dir_sup).
+
+-behaviour(supervisor).
+
+-export([start_link/0, start_child/1]).
+
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+start_link() ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+start_child(DirName) ->
+    supervisor:start_child(?SERVER, [DirName]).
+
+init([]) ->
+    Element = {lc_dir_worker, {lc_dir_worker, start_link, []},
+               temporary, brutal_kill, worker, [lc_dir_worker]},
+    Children = [Element],
+    RestartStrategy = {simple_one_for_one, 0, 1},
+    {ok, {RestartStrategy, Children}}.
+
